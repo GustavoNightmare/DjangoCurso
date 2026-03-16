de37c168f5c1e02e656e6aaa429e300fd074b666
GustavoNightmare <gsandovalm@unicauca.edu.co>
2026-03-16 00:50:15 +0000

    Inicio del curso Django

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpRequest
+
+def helloWorld(request):
+    return HttpResponse('<h1>Hola mundo</h1>')
+
+def Suma(request):
+    listaNumeros= [3,4,7,6,7,9,4,2,10]
+    suma=0
+    for numero in listaNumeros:
+        suma+=numero
+    mensaje= f"<h1> El numero resultado de la suma es = {suma} </h1>"
+    return HttpResponse(mensaje)
+
+
+# Create your views here.
